a6def3737b9ee0af0236ee3989bb79846d95aed4
mfuu <446310957@qq.com>
2022-08-24 14:03:37 +0800

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
 
-export function debounce(func: Function, delay: number = 50, immediate:boolean = false){
+export function debounce(fn: Function, delay: number = 50, immediate:boolean = false){
   let timer: any | undefined
   let result: Function
   let debounced = function(...args: any){
@@ -9,10 +9,10 @@ export function debounce(func: Function, delay: number = 50, immediate:boolean =
       timer = setTimeout(()=>{
         timer = null
       }, delay)
-      if(callNow) result = func.apply(this, args)
+      if(callNow) result = fn.apply(this, args)
     } else{
       timer = setTimeout(()=>{
-        func.apply(this, args)
+        fn.apply(this, args)
       }, delay)
     }
     return result
@@ -25,13 +25,12 @@ export function debounce(func: Function, delay: number = 50, immediate:boolean =
 }
 
 export function throttle(fn: Function, delay: number) {
-  let timer = null
-  return function() {
-    const context = this, args = arguments
+  let timer: any | undefined
+  return function(...args: IArguments[]) {
     if(!timer) {
       timer = setTimeout(function() {
         timer = null
-        fn.apply(context, args)
+        fn.apply(this, args)
       }, delay)
     }
   }
